c6f6e3bc97e9e74c8710e506ce60bfc802d19a05
liutianliang <1076226525@qq.com>
2019-03-20 10:58:24 +0800

    第六天

--- gateway/gateway-web/src/main/java/com/gatewayweb/filter/AccessGatewayFilter.java
+++ gateway/gateway-web/src/main/java/com/gatewayweb/filter/AccessGatewayFilter.java
@@ -1,11 +1,20 @@
 package com.gatewayweb.filter;
 
+import com.authenticationclient.service.IAuthService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+
 
 /**
  * @program: springclouddemo
@@ -17,14 +26,38 @@ import reactor.core.publisher.Mono;
 @ComponentScan(basePackages = "com.springcloud.auth.client")
 public class AccessGatewayFilter implements GlobalFilter {
 
-    private static final String X_CLIENT_USER_TOKEN="x-client-user-token";
+    private static final String X_CLIENT_TOKEN_USER="x-client-user-token";
     private static final String X_CLIENT_TOKEN="x-client-token";
 
-    //private IAuthService
+    @Autowired
+    private IAuthService iAuthService;
 
+    private Mono<Void> unauthorized(ServerWebExchange serverWebExchange){
+        serverWebExchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        DataBuffer buffer= serverWebExchange.getResponse()
+                .bufferFactory().wrap(HttpStatus.UNAUTHORIZED.getReasonPhrase().getBytes());
+        return serverWebExchange.getResponse().writeWith(Flux.just(buffer));
+    }
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-
-        return null;
+        ServerHttpRequest request = exchange.getRequest();
+        String authentication = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        String method = request.getMethodValue();
+        String url = request.getPath().value();
+        log.debug("url:{},method:{},headers:{}", url, method, request.getHeaders());
+        //不需要网关签权的url
+        if (iAuthService.ignoreAuthentication(url)) {
+            return chain.filter(exchange);
+        }
+        //调用签权服务看用户是否有权限，若有权限进入下一个filter
+        if (iAuthService.hasPermission(authentication, url, method)) {
+            ServerHttpRequest.Builder builder = request.mutate();
+            //TODO 转发的请求都加上服务间认证token
+            builder.header(X_CLIENT_TOKEN, "TODO zhoutaoo添加服务间简单认证");
+            //将jwt token中的用户信息传给服务
+            builder.header(X_CLIENT_TOKEN_USER, iAuthService.getJwt(authentication).getClaims());
+            return chain.filter(exchange.mutate().request(builder.build()).build());
+        }
+        return unauthorized(exchange);
     }
 }
